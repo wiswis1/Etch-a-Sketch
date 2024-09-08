bf16567bb5eaf234b50e5e71b26cd787eab19324
wiswis1 <elijahwidom6@gmail.com>
2024-09-08 00:16:06 -0500

    Add hover effect to change color of cell when mouse goes over it Update width and height of the columns and the cells

--- game.js
+++ game.js
@@ -1,24 +1,67 @@
 const gridX = 16;
 const gridY = 16;
-const grid = document.getElementById("grid-container");
+
+var isBlack = true; //default color
+var isCustom = false; //custom color picked by the user
+var isRainbow = false; //random rgb
+var isEraser = false; //eraser
+
+var currentColor;
 
 
+function createGrid() {
+  const grid = document.getElementById("grid-container");
 
-function createGrid(){
-    for(let i = 0; i<gridX;i++){
-        let column = document.createElement('div');
-        column.classList.add('column')
-        column.style.width = 400/gridX+"px";
-        column.style.height = "500px"
-        grid.appendChild(column);
-        for(let j = 0;j<gridY;j++){
-            let cell = document.createElement('div');
-            cell.classList.add("grid-cell");
-            cell.style.width = 400/gridX+"px";
-            cell.style.height = 500/gridY+"px";
-            column.appendChild(cell) 
-        }
+  for (var i = 0; i < gridX; i++) {
+    var column = document.createElement("div");
+    column.classList.add("column");
+    column.style.width = 400 / gridX + "px";
+    column.style.height = "400px";
+    grid.appendChild(column);
+    for (var j = 0; j < gridY; j++) {
+      var gridCell = document.createElement("div");
+      gridCell.classList.add("grid-cell");
+      gridCell.style.width = 400 / gridX + "px";
+      gridCell.style.height = 400 / gridY + "px";
+
+      gridCell.addEventListener("mouseover", changeColor);
+
+      column.appendChild(gridCell);
     }
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function changeColor(e) {
+  if (isBlack) {
+    e.target.style.backgroundColor = "black";
+  } else if (isRainbow) {
+    const R = Math.floor(Math.random()*256);
+    const G = Math.floor(Math.random()*256);
+    const B = Math.floor(Math.random()*256);
+    e.target.style.backgroundColor = `rgb(${R},${G},${B})`
+  }else if(isCustom){
+    e.target.style.backgroundColor = currentColor;
+  } 
+  else if (isEraser){
+    e.target.style.backgroundColor = "rgb(218, 214, 214)";
+  }
+}
+
+
+
+
 
 createGrid();

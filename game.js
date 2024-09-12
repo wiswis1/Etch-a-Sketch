05746ff0182132b952817f91fddbfdd0e4da7573
wiswis1 <elijahwidom6@gmail.com>
2024-09-12 00:02:37 -0500

    Create events for buttons, test buttons to make sure they work. Create function to setMode, resetGrid and changeSize

--- game.js
+++ game.js
@@ -1,28 +1,41 @@
-const gridX = 16;
-const gridY = 16;
+const GRID_SIZE = 16;
 
-var isBlack = true; //default color
-var isCustom = false; //custom color picked by the user
-var isRainbow = false; //random rgb
-var isEraser = false; //eraser
+var currentMode = 'black';
 
-var currentColor;
+var currentSize =  GRID_SIZE;
 
 
-function createGrid() {
-  const grid = document.getElementById("grid-container");
 
-  for (var i = 0; i < gridX; i++) {
+const blackBtn = document.getElementById('blackBtn');
+const colorPickBtn = document.getElementById('colorPickBtn');
+const rgbBtn = document.getElementById('rgbBtn');
+const eraseBtn = document.getElementById('eraseBtn');
+const clearBtn  = document.getElementById('clearBtn');
+const grid = document.getElementById('grid-container');
+// const sizeValue = document.getElementById('sizeValue')
+// const sizeSlider = document.getElementById('sizeSlider')
+
+
+
+blackBtn.onclick = () => setCurrentMode('black');
+colorPickBtn.onclick = (e) => setCurrentMode(e.target.value);
+rgbBtn.onclick = () => setCurrentMode('rgb');
+eraseBtn.onclick = () => setCurrentMode('eraser');
+clearBtn.onclick = () => resetGrid();
+
+
+function createGrid(size) { 
+  for (var i = 0; i < size; i++) {
     var column = document.createElement("div");
     column.classList.add("column");
-    column.style.width = 400 / gridX + "px";
+    column.style.width = 400 / size + "px";
     column.style.height = "400px";
     grid.appendChild(column);
-    for (var j = 0; j < gridY; j++) {
+    for (var j = 0; j < size; j++) {
       var gridCell = document.createElement("div");
       gridCell.classList.add("grid-cell");
-      gridCell.style.width = 400 / gridX + "px";
-      gridCell.style.height = 400 / gridY + "px";
+      gridCell.style.width = 400 / size + "px";
+      gridCell.style.height = 400 / size + "px";
 
       gridCell.addEventListener("mouseover", changeColor);
 
@@ -31,37 +44,48 @@ function createGrid() {
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-function changeColor(e) {
-  if (isBlack) {
-    e.target.style.backgroundColor = "black";
-  } else if (isRainbow) {
-    const R = Math.floor(Math.random()*256);
-    const G = Math.floor(Math.random()*256);
-    const B = Math.floor(Math.random()*256);
-    e.target.style.backgroundColor = `rgb(${R},${G},${B})`
-  }else if(isCustom){
-    e.target.style.backgroundColor = currentColor;
-  } 
-  else if (isEraser){
-    e.target.style.backgroundColor = "rgb(218, 214, 214)";
-  }
+function changeSize(newSize){
+    currentSize = newSize;
+    resetGrid();
 }
 
 
 
 
+function setCurrentMode(newMode){
+    currentMode = newMode;
+}
 
-createGrid();
+
+//make a function to clear the grid(basically resets the game)
+function resetGrid(){
+    grid.innerHTML = '';
+    createGrid(size);
+}
+
+
+function changeColor(e) {
+  if (currentMode === 'black') {
+    e.target.style.backgroundColor = 'black';
+  } else if (currentMode === 'rgb') {
+    const R = Math.floor(Math.random()*256);
+    const G = Math.floor(Math.random()*256);
+    const B = Math.floor(Math.random()*256);
+    e.target.style.backgroundColor = `rgb(${R},${G},${B})`
+  }
+  else if (currentMode === 'eraser'){
+    e.target.style.backgroundColor = null;
+  }
+  else if(isCustom){
+    e.target.style.backgroundColor = currentColor;
+  } 
+
+}
+
+
+
+
+window.onload = () =>{
+    createGrid(currentSize);
+    menu();
+}
